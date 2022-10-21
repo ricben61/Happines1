d5f9a1692c865469d035f8af486e767d30a3ea82
benoit <benoitricoult@gmail.com>
2022-10-21 12:53:41 +0200

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+//componemts//
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+// pages//
+import { AccueilComponent } from './components/pages/accueil/accueil.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { PrestationsComponent } from './components/pages/prestations/prestations.component';
+import { MonparcoursComponent } from './components/pages/monparcours/monparcours.component';
+import { AstucesComponent } from './components/pages/astuces/astuces.component';
+import { OutilsEtMethodesComponent } from './components/pages/outils-et-methodes/outils-et-methodes.component';
+
+ 
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    AccueilComponent,
+    ContactComponent,
+    PrestationsComponent,
+    NavbarComponent,
+    MonparcoursComponent,
+    AstucesComponent,
+    OutilsEtMethodesComponent,
+    
+    
   ],
   imports: [
+    
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    RouterModule
+    
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/pages/outils-et-methodes/outils-et-methodes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-outils-et-methodes',
+  templateUrl: './outils-et-methodes.component.html',
+  styleUrls: ['./outils-et-methodes.component.css']
+})
+export class OutilsEtMethodesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
